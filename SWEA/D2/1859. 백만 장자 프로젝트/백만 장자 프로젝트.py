5705631e42d4fa825c3c4424b9f4246ab5a255be
Shin Yerin <37470054+Yerinnnn@users.noreply.github.com>
2024-11-15 22:22:32 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,095 ms, Memory: 174,900 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -1,16 +1,16 @@
 T = int(input())
+
 for test_case in range(1, T + 1):
     N = int(input())
     prices = list(map(int, input().split()))
-
+    
     max_price = 0
     profit = 0
-
-    # 뒤에서부터 가격을 보면서 최댓값을 업데이트하며 이익 계산
+    
     for price in reversed(prices):
         if price > max_price:
             max_price = price
         else:
             profit += max_price - price
-
+            
     print(f"#{test_case} {profit}")
