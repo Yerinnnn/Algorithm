cb968500727604422571cb8af432baea2b3a525f
Shin Yerin <37470054+Yerinnnn@users.noreply.github.com>
2024-11-16 20:18:44 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 135 ms, Memory: 48,940 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,11 +1,11 @@
 for test_case in range(1, 11):
     N = int(input())
     buildings = list(map(int, input().split()))
-    count = 0
-    
+    view_count = 0  # 조망권 확보 세대의 총합
+
     for i in range(2, N-2):
-        max_building = max(buildings[i-2], buildings[i-1], buildings[i+1], buildings[i+2])
-        if buildings[i] > max_building:
-            count += buildings[i] - max_building
-            
-    print(f"#{test_case} {count}")
+        max_neighbor_height = max(buildings[i-2], buildings[i-1], buildings[i+1], buildings[i+2])
+        if buildings[i] > max_neighbor_height:
+            view_count += buildings[i] - max_neighbor_height  # 조망권 세대 높이 계산
+
+    print(f"#{test_case} {view_count}")
