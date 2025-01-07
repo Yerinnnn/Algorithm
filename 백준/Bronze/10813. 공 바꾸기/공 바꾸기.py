74880077769dd3b4c5750010a6c3e3f5751570bc
Shin Yerin <37470054+Yerinnnn@users.noreply.github.com>
2025-01-07 14:54:06 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,9 @@
+# 바구니의 갯수 N, 공 교환 횟수 M
+N, M = map(int, input().split())
+baskets = [i for i in range(N + 1)]
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    baskets[i], baskets[j] = baskets[j], baskets[i]
+    
+print(*baskets[1:])
