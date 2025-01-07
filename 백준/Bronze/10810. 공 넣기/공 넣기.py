b97544330804f0212db0fff434f761910e718b82
Shin Yerin <37470054+Yerinnnn@users.noreply.github.com>
2025-01-07 14:47:12 +0900

    [Bronze III] Title: 공 넣기, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,11 @@
+# 바구니의 개수 N, 공을 넣는 횟수 M
+N, M = map(int, input().split())
+baskets = [0] * N
+
+for _ in range(M):
+    # i번 바구니부터 j번 바구니까지에 k번 번호가 적혀져 있는 공을 넣는다
+    i, j, k = map(int, input().split())
+    for idx in range(i-1, j):
+        baskets[idx] = k
+
+print(*baskets)
